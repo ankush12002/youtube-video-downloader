97a7aa03fa6e69bbd7f9c8e6f5a9a0d824f80b8c
ankush12002 <ankushkumar120212@gmail.com>
2024-07-19 20:06:44 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+pip install tkinter
+Pip install pytube
+pip install tkinter
+Pip install pytube
+ 
+ #install this into your terminal 
+   
+
+
+root.mainloop()def Downloader():     
+    url =YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x= 180 , y = 210)  
+
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2, command = Downloader).place(x=180 ,y = 150)
+
+root.mainloop()def Downloader():     
+    url =YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x= 180 , y = 210)  
+
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2, command = Downloader).place(x=180 ,y = 150)
+
+root.mainloop()
